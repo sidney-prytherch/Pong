e5ce1f0e53d985607897c0dffde23d9008919704
sidney-prytherch <sidneyprytherch@gmail.com>
2018-03-07 15:08:00 -0500

    updated keydown and keyup logic

--- src/Pong.js
+++ src/Pong.js
@@ -1,21 +1,40 @@
 import Player from './Player.js';
 
 const players = [
-    new Player(1, {up: 87, down: 83}), 
-    new Player(2, {up: 38, down: 40})
+    new Player(1, {up: 87, down: 83}, false),
+    new Player(2, {up: 38, down: 40}, false)
 ];
 
 /**
  * 
  * @param {KeyboardEvent} event 
  */
-document.onkeydown = function(event) {
+document.onkeydown = (event) => {
     event = event || window.event;
     keySearch:
     for (let player of players) {
         for (let property in player.keyMap) {
-            if (event.keyCode === player.keyMap[property]) {
+            if (event.keyCode === player.keyMap[property] && event.keyCode !== player.keyDown) {
+                player.keyDown(event.keyCode);
                 player.setMovementInterval(property);
+                break keySearch;
+            }
+        }
+    }
+}
+
+/**
+ * 
+ * @param {KeyboardEvent} event 
+ */
+document.onkeyup = (event) => {
+    event = event || window.event;
+    keySearch:
+    for (let player of players) {
+        for (let property in player.keyMap) {
+            if (event.keyCode === player.keyMap[property] && event.keyCode === player.keyDown) {
+                player.keyDown(null);
+                player.clearMovementInterval();
                 break keySearch;
             }
         }
